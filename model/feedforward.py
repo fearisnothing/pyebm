b0b95046011be8e177d0fc91f1104af82579c161
d.warde.farley <d.warde.farley@2d4c95bb-fc54-0410-9a92-c9fd0c275703>
2008-08-21 21:06:05 +0000

    Importing some old code

---
+++ model/feedforward.py
@@ -0,0 +1,136 @@
+import numpy as N
+import scipy as S
+
+class PreTrainedAutoencoder:
+	def __init__(self, trainedRBMs):
+	    params = []
+	    for rbm in trainedRBMs:
+	        params.append(N.array(rbm._vishid.flat))
+	        params.append(N.array(rbm._hidbiases.flat))
+	    for rbm in trainedRBMs[::-1]:
+	        params.append(N.array(rbm._vishid.T.flat))
+	        params.append(N.array(rbm._visbiases.flat))
+	    
+	    self._allparams = N.concatenate(params)
+	    self._weights = []
+	    self._biases = []
+	    offset = 0
+	    for rbm in trainedRBMs:
+	        top = offset + rbm._numhid * rbm._numvis
+	        weights = N.asarray(N.reshape(self._allparams[offset:top], 
+	            (rbm._numvis, rbm._numhid)))
+	        offset = top
+	        top = offset + rbm._numhid
+	        biases = N.asarray(self._allparams[offset:top])
+	        self._weights.append(weights)
+	        self._biases.append(biases)
+	        offset = top
+	    for rbm in trainedRBMs[::-1]:
+	        top = offset + rbm._numhid * rbm._numvis
+	        weights = N.asarray(N.reshape(self._allparams[offset:top], 
+	            (rbm._numhid, rbm._numvis)))
+	        offset = top
+	        top = offset + rbm._numvis
+	        biases = N.asarray(self._allparams[offset:top])
+	        offset = top
+	        self._weights.append(weights)
+	        self._biases.append(biases)
+	        
+	def eval(self, data, returninputoutput=False):
+	    states = data
+	    layerinputs = []
+	    layeroutputs = []
+	    for i in xrange(len(self._weights) / 2):
+	        if i > 0:
+	            #print "sigmoid on " + str(states.shape)
+	            states = 1. / (1 + N.exp(-states))
+	            layeroutputs.append(states)
+	        #print "passing through linearity %s * %s (states * weights)" % (str(states.shape), str(self._weights[i].shape))
+	        states = N.dot(states,self._weights[i]) + self._biases[i][N.newaxis,:]
+	        layerinputs.append(states)
+	    layeroutputs.append(states)
+	    for i in xrange(len(self._weights) / 2, len(self._weights)):
+	        states = N.dot(states,self._weights[i]) + self._biases[i][N.newaxis,:]
+	        layerinputs.append(states)
+	        #print "passing through linearity %s * %s (states * weights)" % (str(states.shape), str(self._weights[i].shape))
+	        states = 1. / (1 + N.exp(-states))
+	        layeroutputs.append(states)
+	        
+	        #print "sigmoid on " + str(states.shape)
+	    if returninputoutput:
+	        return layerinputs, layeroutputs
+	    else:
+	        return states
+	
+	def _error(self, params, data):
+	    params_save = N.copy(self._allparams)
+	    self._allparams.flat = params.flat
+	    m = self.eval(data)
+	    if N.any(m.flat == 0):
+	        m.flat[m.flat == 0] = 1.0e-8
+	    if N.any(m.flat == 1):
+	        m.flat[m.flat == 1] = 1 - 1.0e-8
+	    E = -N.sum(data * N.log(m) + (1 - data)*N.log(1 - m))
+	    self._allparams.flat = params_save.flat
+	    del params_save
+	    return E
+	    	
+	def _grad(self, params, data):
+	    params_save = N.copy(self._allparams)
+	    self._allparams.flat = params.flat
+	    layerinputs,layeroutputs = self.eval(data,True)
+	    delta = [None] * len(self._weights)
+	    grads = [None] * len(self._weights)
+	    grads_biases = [None] * len(self._weights)
+	    all_grads = [None] * 2 * len(self._weights)
+	    for i in xrange(len(self._weights)-1,len(self._weights)/2-1,-1):
+	        if i == len(self._weights) - 1:
+	            delta[i] = layeroutputs[i] - data
+	        else:
+	            sig = (1 + N.exp(-layerinputs[i]))**-1
+	            hprime = sig * (1 - sig)
+	            delta[i] = hprime * N.dot(delta[i+1], self._weights[i+1].T)
+	    for i in xrange(len(self._weights)/2 - 1, -1, -1):
+	        if i == len(self._weights)/2 - 1:
+	            delta[i] = N.dot(delta[i+1], self._weights[i+1].T)
+	        else:
+	            sig = (1 + N.exp(-layerinputs[i]))**-1
+	            hprime = sig * (1 - sig)
+	            delta[i] = hprime * N.dot(delta[i+1], self._weights[i+1].T)
+	    for i in xrange(len(self._weights)):
+	        if i == 0:
+	            grads[i] =  N.dot(data.T,delta[i])
+	        else:
+	            grads[i] = N.dot(layeroutputs[i-1].T,delta[i])
+	        grads_biases[i] = N.sum(delta[i],axis=0)
+	        
+	    for i in xrange(len(grads)):
+	        all_grads[2*i] = grads[i]
+	        all_grads[2*i+1] = grads_biases[i]
+	    
+	    self._allparams.flat = params_save.flat
+	    del params_save
+	    return N.concatenate([x.flat for x in all_grads])
+	
+	def tune(self, batchdata, valid, numruns, maxiter):
+	    validerrors = []
+	    for run in xrange(numruns):
+	        for batch in xrange(batchdata.shape[2]):
+	            print "Run %d, batch %d... " % (run, batch)
+	            self.all_params = S.optimize.fmin_cg(self._error, self._allparams, 
+	                self._grad, (batchdata[:,:,batch],), maxiter=maxiter)
+	        validerrors.append(self._error(self._allparams, valid))
+	        print "Validation error: " + str(validerrors[-1])
+	        if run > 0 and validerrors[-1] > validerrors[-2]:
+	            print "Validation error increased!"
+	            break
+	
+	def encode(self, data):
+	    states = data
+	    for i in xrange(len(self._weights) / 2):
+	        if i > 0:
+	            #print "sigmoid on " + str(states.shape)
+	            states = 1. / (1 + N.exp(-states))
+	        #print "passing through linearity %s * %s (states * weights)" % (str(states.shape), str(self._weights[i].shape))
+	        states = N.dot(states,self._weights[i]) + self._biases[i][N.newaxis,:]
+	    return states
---
+++ model/logreg.py
@@ -0,0 +1,108 @@
+"""
+Dirt simple binary logistic regression routines written with numpy/scipy.
+Uses scipy.optimize.fmin_cg for the nonlinear optimization. 
+
+TODO: 
+    * Object-orient the whole thing (maybe; I like the simplicity of it now)
+    * Fallback to gradient descent if scipy isn't available.
+    * Come up with a test scenario to run if called at the command line.
+
+By David Warde-Farley, April 2008
+
+$Id: logreg.py 52 2008-04-16 07:21:41Z dwf $
+"""
+
+import numpy as N
+import scipy as S
+from scipy import optimize as opt
+
+def lr_probs(coeffs, data):
+    """
+    Estimate p(target = 1) using the given coefficients.
+     * coeffs is a 1-dimensional (D+1)-length array.
+     * data is assumed to be N x D design matrix (no column of 1's).
+    """
+    bias = coeffs[0]
+    coeffs = coeffs[1:]
+    eta = bias + N.sum(coeffs * data, axis=1)
+    probs = 1. / (1. + N.exp(-eta))
+    return probs
+    
+def lr_nloglik(coeffs, data, target, decay=0):
+    """
+    Calculate the negative log likelihood of a dataset with given targets.
+     * coeffs is a 1-dimensional (D+1)-length array.
+     * data is assumed to be N x D design matrix (no column of 1's).
+     * target is a binary 1D array of length N.
+     * decay is an optional parameter controlling the amount of weight decay
+       (regularization).
+    """
+    probs = lr_probs(coeffs, data)
+    bias = coeffs[0]
+    coeffs = coeffs[1:]
+    L = N.sum(target * N.log(probs)) + N.sum((1 - target) * N.log(1 - probs))
+    if decay:
+        L = L - decay * (bias**2 + N.sum(coeffs**2))
+    return -L
+
+def lr_nloglik_grad(coeffs, data, target, decay=None):
+    """
+    Calculate the gradient of the negative log likelihood.
+    * coeffs is a 1-dimensional (D+1)-length array.
+    * data is assumed to be N x D design matrix (no column of 1's).
+    * target is a binary 1D array of length N.
+    * decay is an optional parameter controlling the amount of weight decay
+      (regularization).
+    """
+    probs = lr_probs(coeffs, data)
+    augdata = N.concatenate((N.ones((data.shape[0],1)),data),axis=1)
+    grads = -N.sum((target - probs)[:,N.newaxis] * augdata, axis=0)
+    if decay:
+        grads = grads + 2*decay*coeffs
+    return grads
+
+def lr_fit(data, target, decay=None, maxiter=2000, callback=None, randinit=False):
+    """
+    Fit a binary logistic regression model to the given dataset and targets.
+    * data is assumed to be N x D design matrix (no column of 1's).
+    * target is a binary 1D array of length N.
+    * decay is an optional parameter controlling the amount of weight decay
+      (regularization). Defaults to none/zero.
+    * maxiter is an optional parameter passed along to scipy.optimize.fmin_cg.
+      Defaults to 20000.
+    """
+    #random initialization? 
+    if randinit:
+        initial = S.randn(data.shape[1] + 1) * 0.01
+    else:
+        initial = N.zeros(data.shape[1] + 1)
+    beta = opt.fmin_cg(lr_nloglik, initial, lr_nloglik_grad, \
+        args=(data,target,decay), maxiter=maxiter,callback=callback)
+    return beta
+
+def lr_eval(coeffs, data, target, thresh=0.5):
+    probs = lr_probs(coeffs, data)
+    predictions = probs > thresh
+    disagreements = N.where(predictions != target)[0]
+    falsenegs = len(N.where(target[disagreements] == True)[0])
+    falsepos = len(N.where(target[disagreements] == False)[0])
+    trueneg = len(N.where(target[predictions == target] == False)[0])
+    truepos = len(N.where(target[predictions == target] == True)[0])
+    actualpos = len(N.where(target == True)[0])
+    actualneg = len(N.where(target == False)[0])
+    predpos = len(N.where(predictions == True)[0])
+    predneg = len(N.where(predictions == False)[0])
+    
+    precision = float(truepos) / predpos
+    recall = float(truepos) / actualpos
+    specificity = float(trueneg) / actualneg
+    accuracy = (target.shape[0] - float(len(disagreements))) / target.shape[0]
+    print float(len(disagreements))
+    
+    return dict(precision=precision, recall=recall, 
+        specificity=specificity, accuracy=accuracy)
+    
+    
+if __name__ == "__main__":
+    print "Punt!"
+    pass
